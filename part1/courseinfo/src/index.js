43fc1a1b49dabeb2359c65b3f8f1ed806b2abc4a
Tamás <molnart90@gmail.com>
2019-06-10 16:37:02 +0200

    Use components in courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Content = (props) => {
+  return (
+   <>
+   {props.part} {" "}
+   </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+     {props.nr}
+    <br/>
+    <p></p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} />
+      <Total nr={exercises1} />
+      <Content part={part2} />
+      <Total nr={exercises2} />
+      <Content part={part3} />
+      <Total nr={exercises3} />
+      Number of exercises {exercises1 + exercises2 + exercises3}
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
