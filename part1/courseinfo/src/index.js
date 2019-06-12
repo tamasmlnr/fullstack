6b22471503e5f989a4ba2dc216bb7072906fef87
Tamás <molnart90@gmail.com>
2019-06-12 21:28:05 +0200

    Use classes in courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,55 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part title={props.part1} nr ={props.nr1} />
-      <Part title={props.part2} nr ={props.nr2}/>
-      <Part title={props.part3} nr ={props.nr3}/>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-    {props.title} {" "}
-    {props.nr}
-    <p></p>
-    </>
-  )
-
-}
-const Total = (props) => {
-  return (
-    <>
-     {props.text}  {" "}
-     {props.nr}
-    </>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const part4= {
+    name: 'Number of exercises',
+    exercises: part1.exercises+part2.exercises+part3.exercises
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} nr1={exercises1} part2={part2} nr2={exercises2} part3={part3} nr3={exercises3} />
-     <Total text="Number of exercises" nr={exercises1 + exercises2 + exercises3}/>
+      <h1>{course}</h1>
+      {part1.name} {part1.exercises} <br/>
+      {part2.name} {part2.exercises} <br/>
+      {part3.name} {part3.exercises} <br/>
+      {part4.name} {part4.exercises} <br/>
     </div>
   )
 }
