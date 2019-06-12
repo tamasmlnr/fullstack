4f80b600534c270ea7ea1c001b26069e19be6858
Tamás <molnart90@gmail.com>
2019-06-12 21:34:38 +0200

    courseinfo step4 - array of objects

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,31 +3,27 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const part4= {
-    name: 'Number of exercises',
-    exercises: part1.exercises+part2.exercises+part3.exercises
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    },
+   ]
 
   return (
     <div>
       <h1>{course}</h1>
-      {part1.name} {part1.exercises} <br/>
-      {part2.name} {part2.exercises} <br/>
-      {part3.name} {part3.exercises} <br/>
-      {part4.name} {part4.exercises} <br/>
+      {parts[0].name} {parts[0].exercises} <br/>
+      {parts[1].name} {parts[1].exercises} <br/>
+      {parts[2].name} {parts[2].exercises} <br/>
     </div>
   )
 }
