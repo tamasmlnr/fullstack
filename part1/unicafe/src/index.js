d32409dc8c2565ccd11379a2181ed996f3a9cd78
Tamás <molnart90@gmail.com>
2019-06-17 20:49:19 +0200

    Add rating components/attributes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom'
 
 const Header = ({title}) => <div> <h1>{title}</h1> </div>
 
+const Button = ({handleClick, text}) =>  {
+  return <button onClick={handleClick}>
+    {text}
+  </button>
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return <div>
+    good: {good} <br/>
+    neutral: {neutral}<br/>
+    bad: {bad}<br/>
+
+  </div>
+}
 
 const App = () => {
   // save clicks of each button to own state
@@ -10,10 +24,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = () => {
+    setGood(good+1)
+  }
+
+  const addNeutral = () => {
+    setNeutral(neutral+1)
+  }
+
+  const addBad = () => {
+    setBad(bad+1)
+  }
+
   return (
     <div>
     <Header title="give feedback"></Header>
-    <Header title="give feedback"></Header>
+    <Button handleClick={addGood} text="good"></Button>
+    <Button handleClick={addNeutral} text="neutral"></Button>
+    <Button handleClick={addBad} text="bad"></Button>
+    <Header title="statistics"></Header>
+    <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
