bfcbc00466f63f902209618352492b70da04e69d
Tamás <molnart90@gmail.com>
2019-06-23 18:44:15 +0200

    Add name submitting

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react'
 
+const Phonebook = ({people}) => {
+  return people.map(p => <Person name={p.name} key={p.name}></Person>)
+}
+
+const Person = ({name}) => {
+  return <>{name}<br/></>
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      id: 1}
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+ const submitName = (event) => {
+ event.preventDefault()
+  const nameToAdd = {name: newName, id:persons.length+1}
+  setPersons(persons.concat(nameToAdd))
+}
+
   return (
     <div>
+      <div>debug: {newName}</div>   
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={submitName}>
         <div>
-          name: <input />
+          name: 
+          <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Phonebook people={persons}/>
     </div>
   )
 }
