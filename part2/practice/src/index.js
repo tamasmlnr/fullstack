9b173fac117e7f29871b75004f582a9d7140b125
Tamás <molnart90@gmail.com>
2019-06-26 17:09:49 +0200

    Add Axios and json-server dependencies

--- part2/practice/src/index.js
+++ part2/practice/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+promise.then(response => {
+  console.log(response)
+})
 
 const notes = [
   {
@@ -21,22 +33,6 @@ const notes = [
     important: true
   }
 ]
-
-const result = notes.map(note => {note.content} )
-console.log(result)
-
-const App = (props) => {
-
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {result}
-      </ul>
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App notes={notes} />,
