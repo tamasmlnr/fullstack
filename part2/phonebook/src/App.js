8fc84a7b5ba7a02e3a38316b97b4fc630818ac3c
Tamás <molnart90@gmail.com>
2019-06-26 18:52:45 +0200

    Add axios, use effect hook to get data from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 const Filter = ({searchValue}) => {
   return (
@@ -40,17 +41,22 @@ const Person = ({ name, number }) => {
   );
 };
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
 
+const App = () => {
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterWord, setFilterWord] = useState("");
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      console.log("kaki")
+      setPersons(response.data)
+    })
+  }, [])
+
 
   const handleChange = event => {
     setNewName(event.target.value);
